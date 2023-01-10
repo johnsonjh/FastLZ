0477cfce746f974ad44759df12ed4eb046c25f12
Jeffrey H. Johnson <trnsz@pobox.com>
2023-01-09 23:41:06 -0500

    Remove deprecated function; add return codes

--- fastlz.c
+++ fastlz.c
@@ -1,3 +1,6 @@
+#pragma clang diagnostic push
+// On Windows builds some macros resolve to empty statements
+#pragma ide diagnostic ignored "EmptyDeclOrStmt"
 /*
   FastLZ - Byte-aligned LZ77 compression library
   Copyright (C) 2005-2020 Ariya Hidayat <ariya.hidayat@gmail.com>
@@ -53,10 +56,19 @@
 #endif
 
 #if defined(FASTLZ_SAFE)
-#define FASTLZ_BOUND_CHECK(cond) \
-  if (FASTLZ_UNLIKELY(!(cond))) return 0;
+#define FASTLZ_BOUND_CHECK_OOB(cond) \
+  if (FASTLZ_UNLIKELY(!(cond))) return FASTLZ_ERROR_TOO_SMALL;
 #else
-#define FASTLZ_BOUND_CHECK(cond) \
+#define FASTLZ_BOUND_CHECK_OOB(cond) \
+  do {                           \
+  } while (0)
+#endif
+
+#if defined(FASTLZ_SAFE)
+#define FASTLZ_BOUND_CHECK_CORRUPT(cond) \
+  if (FASTLZ_UNLIKELY(!(cond))) return FASTLZ_ERROR_CORRUPT;
+#else
+#define FASTLZ_BOUND_CHECK_CORRUPT(cond) \
   do {                           \
   } while (0)
 #endif
@@ -87,7 +99,7 @@ static void fastlz_memmove(uint8_t* dest, const uint8_t* src, uint32_t count) {
           *dest++ = *src++;
         } while (--count);
         break;
-      case 3:
+      case 3: // NOLINT(bugprone-branch-clone)
         *dest++ = *src++;
       case 2:
         *dest++ = *src++;
@@ -346,19 +358,19 @@ int fastlz1_decompress(const void* input, int length, void* output, int maxout) 
       uint32_t ofs = (ctrl & 31) << 8;
       const uint8_t* ref = op - ofs - 1;
       if (len == 7 - 1) {
-        FASTLZ_BOUND_CHECK(ip <= ip_bound);
+        FASTLZ_BOUND_CHECK_CORRUPT(ip <= ip_bound);
         len += *ip++;
       }
       ref -= *ip++;
       len += 3;
-      FASTLZ_BOUND_CHECK(op + len <= op_limit);
-      FASTLZ_BOUND_CHECK(ref >= (uint8_t*)output);
+      FASTLZ_BOUND_CHECK_OOB(op + len <= op_limit);
+      FASTLZ_BOUND_CHECK_CORRUPT(ref >= (uint8_t*)output);
       fastlz_memmove(op, ref, len);
       op += len;
     } else {
       ctrl++;
-      FASTLZ_BOUND_CHECK(op + ctrl <= op_limit);
-      FASTLZ_BOUND_CHECK(ip + ctrl <= ip_limit);
+      FASTLZ_BOUND_CHECK_OOB(op + ctrl <= op_limit);
+      FASTLZ_BOUND_CHECK_CORRUPT(ip + ctrl <= ip_limit);
       fastlz_memcpy(op, ip, ctrl);
       ip += ctrl;
       op += ctrl;
@@ -494,7 +506,7 @@ int fastlz2_decompress(const void* input, int length, void* output, int maxout) 
 
       uint8_t code;
       if (len == 7 - 1) do {
-          FASTLZ_BOUND_CHECK(ip <= ip_bound);
+          FASTLZ_BOUND_CHECK_CORRUPT(ip <= ip_bound);
           code = *ip++;
           len += code;
         } while (code == 255);
@@ -505,20 +517,20 @@ int fastlz2_decompress(const void* input, int length, void* output, int maxout) 
       /* match from 16-bit distance */
       if (FASTLZ_UNLIKELY(code == 255))
         if (FASTLZ_LIKELY(ofs == (31 << 8))) {
-          FASTLZ_BOUND_CHECK(ip < ip_bound);
+          FASTLZ_BOUND_CHECK_CORRUPT(ip < ip_bound);
           ofs = (*ip++) << 8;
           ofs += *ip++;
           ref = op - ofs - MAX_L2_DISTANCE - 1;
         }
 
-      FASTLZ_BOUND_CHECK(op + len <= op_limit);
-      FASTLZ_BOUND_CHECK(ref >= (uint8_t*)output);
+      FASTLZ_BOUND_CHECK_OOB(op + len <= op_limit);
+      FASTLZ_BOUND_CHECK_CORRUPT(ref >= (uint8_t*)output);
       fastlz_memmove(op, ref, len);
       op += len;
     } else {
       ctrl++;
-      FASTLZ_BOUND_CHECK(op + ctrl <= op_limit);
-      FASTLZ_BOUND_CHECK(ip + ctrl <= ip_limit);
+      FASTLZ_BOUND_CHECK_OOB(op + ctrl <= op_limit);
+      FASTLZ_BOUND_CHECK_CORRUPT(ip + ctrl <= ip_limit);
       fastlz_memcpy(op, ip, ctrl);
       ip += ctrl;
       op += ctrl;
@@ -531,14 +543,6 @@ int fastlz2_decompress(const void* input, int length, void* output, int maxout) 
   return op - (uint8_t*)output;
 }
 
-int fastlz_compress(const void* input, int length, void* output) {
-  /* for short block, choose fastlz1 */
-  if (length < 65536) return fastlz1_compress(input, length, output);
-
-  /* else... */
-  return fastlz2_compress(input, length, output);
-}
-
 int fastlz_decompress(const void* input, int length, void* output, int maxout) {
   /* magic identifier for compression level */
   int level = ((*(const uint8_t*)input) >> 5) + 1;
@@ -547,12 +551,14 @@ int fastlz_decompress(const void* input, int length, void* output, int maxout) {
   if (level == 2) return fastlz2_decompress(input, length, output, maxout);
 
   /* unknown level, trigger error */
-  return 0;
+  return FASTLZ_ERROR_UNKNOWN_LEVEL;
 }
 
 int fastlz_compress_level(int level, const void* input, int length, void* output) {
   if (level == 1) return fastlz1_compress(input, length, output);
   if (level == 2) return fastlz2_compress(input, length, output);
 
-  return 0;
+  return FASTLZ_ERROR_UNKNOWN_LEVEL;
 }
+
+#pragma clang diagnostic pop
--- fastlz.h
+++ fastlz.h
@@ -30,6 +30,10 @@
 #define FASTLZ_VERSION_MINOR 5
 #define FASTLZ_VERSION_REVISION 0
 
+#define FASTLZ_ERROR_CORRUPT -1
+#define FASTLZ_ERROR_TOO_SMALL -2
+#define FASTLZ_ERROR_UNKNOWN_LEVEL -3
+
 #define FASTLZ_VERSION_STRING "0.5.0"
 
 #if defined(__cplusplus)
@@ -37,58 +41,72 @@ extern "C" {
 #endif
 
 /**
-  Compress a block of data in the input buffer and returns the size of
-  compressed block. The size of input buffer is specified by length. The
-  minimum input buffer size is 16.
-
-  The output buffer must be at least 5% larger than the input buffer
-  and can not be smaller than 66 bytes.
-
-  If the input is not compressible, the return value might be larger than
-  length (input buffer size).
-
-  The input buffer and the output buffer can not overlap.
-
-  Compression level can be specified in parameter level. At the moment,
-  only level 1 and level 2 are supported.
-  Level 1 is the fastest compression and generally useful for short data.
-  Level 2 is slightly slower but it gives better compression ratio.
-
-  Note that the compressed data, regardless of the level, can always be
-  decompressed using the function fastlz_decompress below.
+ * @brief Compress data
+ * @details
+ * Compress a block of data in the input buffer and returns the size of
+ * compressed block. The size of input buffer is specified by length. The
+ * minimum input buffer size is 16.
+ *
+ * The output buffer must be at least 5% larger than the input buffer
+ * and can not be smaller than 66 bytes.
+ *
+ * If the input is not compressible, the return value might be larger than
+ * length (input buffer size).
+ *
+ * The input buffer and the output buffer can not overlap.
+ *
+ * Compression level can be specified in parameter level. At the moment,
+ * only level 1 and level 2 are supported.
+ * Level 1 is the fastest compression and generally useful for short data.
+ * Level 2 is slightly slower but it gives better compression ratio. If
+ * any other level is specified, #FASTLZ_ERROR_UNKNOWN_LEVEL is returned.
+ *
+ * Note that the compressed data, regardless of the level, can always be
+ * decompressed using the function fastlz_decompress below.
+ *
+ * @param level compression level. 1 or 2.
+ * @param input data to compress
+ * @param length length of input
+ * @param output receives compressed data
+ * @retval 0 compressed okay
+ * @retval #FASTLZ_ERROR_CORRUPT data could not be encoded
+ * @retval #FASTLZ_ERROR_TOO_SMALL input or output buffer is too small
+ * @retval #FASTLZ_ERROR_UNKNOWN_LEVEL level not one or two
 */
 
 int fastlz_compress_level(int level, const void* input, int length, void* output);
 
 /**
-  Decompress a block of compressed data and returns the size of the
-  decompressed block. If error occurs, e.g. the compressed data is
-  corrupted or the output buffer is not large enough, then 0 (zero)
-  will be returned instead.
 
-  The input buffer and the output buffer can not overlap.
-
-  Decompression is memory safe and guaranteed not to write the output buffer
-  more than what is specified in maxout.
-
-  Note that the decompression will always work, regardless of the
-  compression level specified in fastlz_compress_level above (when
-  producing the compressed block).
  */
 
-int fastlz_decompress(const void* input, int length, void* output, int maxout);
-
 /**
-  DEPRECATED.
-
-  This is similar to fastlz_compress_level above, but with the level
-  automatically chosen.
-
-  This function is deprecated and it will be completely removed in some future
-  version.
-*/
-
-int fastlz_compress(const void* input, int length, void* output);
+ * @brief Decompress data
+ * @details
+ * Decompress a block of compressed data and returns the size of the
+ * decompressed block. If error occurs, e.g. the compressed data is
+ * corrupted or the output buffer is not large enough, then a non-zero
+ * error code is returned.
+ *
+ * The input buffer and the output buffer can not overlap.
+ *
+ * Decompression is memory safe and guaranteed not to write the output buffer
+ * more than what is specified in maxout.
+ *
+ * Note that the decompression will always work, regardless of the
+ * compression level specified in fastlz_compress_level above (when
+ * producing the compressed block).
+ *
+ * @param input data to decompress
+ * @param length length of input in bytes
+ * @param output receives decompressed data
+ * @param maxout size of output in bytes
+ * @retval 0 success
+ * @retval #FASTLZ_ERROR_CORRUPT input is corrupt
+ * @retval #FASTLZ_ERROR_TOO_SMALL input or output buffer is too small
+ * @retval #FASTLZ_ERROR_UNKNOWN_LEVEL not a known compression level
+ */
+int fastlz_decompress(const void* input, int length, void* output, int maxout);
 
 #if defined(__cplusplus)
 }
